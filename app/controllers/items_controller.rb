280541a8637fb674112487b9a05144e756417c19
keiko0712 <kik19860712@gmail.com>
2021-06-16 15:49:36 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :find_item, only: [:show, :edit, :update]
-  before_action :redirect, only: [:edit, :update]
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
+  before_action :redirect, only: [:edit, :update, :destroy]
 
   def new
     @item = Item.new
@@ -37,8 +37,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+   @item.destroy
     redirect_to root_path
   end
 
