63228676168bc92b890e3ce245f7847268e62739
keiko0712 <kik19860712@gmail.com>
2021-07-02 11:17:33 +0900

    マイグレーションファイル編集

--- db/schema.rb
+++ db/schema.rb
@@ -58,15 +58,13 @@ ActiveRecord::Schema.define(version: 2021_06_22_063258) do
   end
 
   create_table "shipping_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "postal_code"
-    t.integer "shipping_area_id"
-    t.string "municipality"
-    t.string "address"
+    t.string "postal_code", null: false
+    t.integer "shipping_area_id", null: false
+    t.string "municipality", null: false
+    t.string "address", null: false
     t.string "building_name"
-    t.string "phone_number"
+    t.string "phone_number", null: false
     t.bigint "purchase_record_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.index ["purchase_record_id"], name: "index_shipping_addresses_on_purchase_record_id"
   end
 
@@ -90,4 +88,7 @@ ActiveRecord::Schema.define(version: 2021_06_22_063258) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
+  add_foreign_key "purchase_records", "items"
+  add_foreign_key "purchase_records", "users"
+  add_foreign_key "shipping_addresses", "purchase_records"
 end
